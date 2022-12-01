7cb05a265d4459a496d48193ac6c4b9787fc489b
AbdulMoizJawed <86627657+AbdulMoizJawed@users.noreply.github.com>
2022-12-01 16:44:19 +0500

    Aesthtical

--- dev/pages/asthetical/index.jsx
+++ dev/pages/asthetical/index.jsx
@@ -1,10 +1,14 @@
 import Asthetical from 'modules/asthetical'
+import Footer from 'layout/footer'
+import Header from 'layout/header'
 import React from 'react'
 
 const index = () => {
   return (
     <>
-        <Asthetical />
+      <Header />
+      <Asthetical />
+      <Footer />
     </>
   )
 }
